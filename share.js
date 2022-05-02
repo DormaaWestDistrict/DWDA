bbe5df7921ab3049be10353320a308e7a8df36ca
MARFO ROBERT <84254917+DormaaWestDistrict@users.noreply.github.com>
2022-05-02 09:09:21 +0100

    Add files via upload

---
+++ share.js
@@ -0,0 +1,46 @@
+
+const whatsapp = document.querySelector('.whatsapp')
+const twitter = document.querySelector('.twitter')
+const facebook = document.querySelector('.facebook')
+const telegram = document.querySelector('.telegram')
+const youtube = document.querySelector('.youtube')
+
+
+const pageUrl=location.href
+const message ='This is an awesome news,kindly take 5 minutes of your time to read it'
+
+const whatsappApi ='https://wa.me/?text=$(pageUrl) .$(message)';
+const facebookApi =  ;
+const twitterApi ='https://twitter.com/intent/tweet?text=$(pageUrl). $(message)'
+const telegramApi ='https://t.me/share/url?url=$(pageUrl)&text=&(message)';
+const youtubeApi =  ;
+
+
+
+whatsapp.addEventListener('click',() => {
+window.open(url= whatsappApi,target='blank')	
+		
+})
+
+twitter.addEventListener('click',() => {
+window.open(url= twitterApi,target='blank')	
+		
+})
+
+facebook.addEventListener('click',() => {
+window.open(url= facebookApi,target='blank')	
+		
+})
+
+telegram.addEventListener('click',() => {
+window.open(url= telegramApi,target='blank')	
+		
+})
+
+youtube.addEventListener('click',() => {
+window.open(url= youtubeApi,target='blank')	
+		
+})
+
+
+
